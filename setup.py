8c4e1989fc1e10c3012dfe910e41596903e68f1b
Edward Z. Yang <ezyang@fb.com>
2017-09-18 19:06:48 -0700

    Verifier for model export / formally create onnx_pytorch.
    onnx_pytorch.verify contains the distilled knowledge about common ways
    model verification can fail, based on our experiences working on
    ONNX exports of models. A few key features:
    
    - It will run your model multiple times with different inputs,
      and give detailed information if (a) the model structure changes,
      (b) the embedded parameters change, (c) the Caffe2/PyTorch output
      results change.
    
    - There is a pretty nice error collecting class which supports
      recovery (so that we can report all errors, instead of bailing
      out at the first one) and context (so that you can easily
      attach messages to all errors.) For technical reasons we
      can't easily put it in a file of its own.
    
    We also give the expect test suite good exercise, since we can easily
    test for error messages and see how improvements affect what is printed.
    
    Requires a PyTorch update tracked at
    https://github.com/pytorch/pytorch/pull/2781
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

---
+++ setup.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import sys
+from setuptools import setup, find_packages
+
+setup(
+    name="onnx-pytorch",
+    version='0.l',
+    description="PyTorch helpers for working with Open Neural Network Exchange format",
+    install_requires=['numpy', 'onnx'],
+    setup_requires=[],
+    tests_require=['onnx-caffe2'],
+    packages=find_packages(),
+    author='ezyang',
+    author_email='ezyang@fb.com',
+    url='https://github.com/ezyang/onnx-pytorch',
+)
