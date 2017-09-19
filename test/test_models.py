c10fa54eea1bf3354a249c88d277322ac71ede81
Edward Z. Yang <ezyang@fb.com>
2017-09-19 10:58:14 -0700

    New expect format for model tests.
    Instead of printing the trace (which can't be conveniently gotten
    via the torch.onnx.export interface), we instead use onnx.helper.printable_graph
    to print it.  This reduces the test dependence on JIT code which seems
    to be a good thing.
    
    There's also a PyTorch submodule update so we get some fixes in primspecs
    that we need, and a onnx-caffe2 update to handle ConvPool padding.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- test/test_models.py
+++ test/test_models.py
@@ -1,8 +1,3 @@
-import torch
-import torch.jit
-from torch.autograd import Variable
-from test_common import TestCase, run_tests, skipIfNoLapack
-
 from model_defs.alexnet import AlexNet
 from model_defs.mnist import MNIST
 from model_defs.word_language_model import RNNModel
@@ -16,8 +11,18 @@ from model_defs.srresnet import SRResNet
 from model_defs.dcgan import _netD, _netG, weights_init, bsz, imgsz, nz
 from model_defs.op_test import DummyNet, ConcatNet, PermuteNet, PReluNet
 
+from test_common import TestCase, run_tests, skipIfNoLapack
+
+import torch
+import torch.onnx
+from torch.autograd import Variable
+
 import onnx
+
 import google.protobuf.text_format
+
+import io
+import unittest
 
 torch.set_default_tensor_type('torch.FloatTensor')
 
@@ -31,125 +36,109 @@ else:
 BATCH_SIZE = 2
 
 
+def export_to_string(model, inputs, *args, **kwargs):
+    f = io.BytesIO()
+    torch.onnx.export(model, inputs, f, *args, **kwargs)
+    return f.getvalue()
+
+
 class TestModels(TestCase):
     maxDiff = None
 
-    def assertONNXExpected(self, binary_pb, subname=None):
+    def exportTest(self, model, inputs, subname=None):
+        binary_pb = export_to_string(model, inputs, export_params=False)
         graph_def = onnx.GraphProto.FromString(binary_pb)
-        self.assertExpected(google.protobuf.text_format.MessageToString(graph_def, float_format='.15g'), subname)
+        # NB: We prefer to look at printable_graph, but it doesn't print
+        # all information.  The pbtxt is the *source of truth*.
+        self.assertExpected(onnx.helper.printable_graph(graph_def), subname)
+        if subname is None:
+            pbtxt_subname = "pbtxt"
+        else:
+            pbtxt_subname = "{}-pbtxt".format(subname)
+        self.assertExpected(google.protobuf.text_format.MessageToString(graph_def, float_format='.15g'), pbtxt_subname)
 
     def test_ops(self):
         x = Variable(
             torch.randn(BATCH_SIZE, 3, 224, 224).fill_(1.0), volatile=True
         )
-        trace, _ = torch.jit.record_trace(toC(DummyNet()), toC(x))
-        self.assertExpected(str(trace))
-        self.assertONNXExpected(trace.export(False), "pbtxt")
+        self.exportTest(toC(DummyNet()), toC(x))
 
     def test_prelu(self):
         x = Variable(
             torch.randn(BATCH_SIZE, 3, 224, 224).fill_(1.0), volatile=True
         )
-        trace, _ = torch.jit.record_trace(PReluNet(), x)
-        self.assertExpected(str(trace))
-        self.assertONNXExpected(trace.export(False), "pbtxt")
+        self.exportTest(PReluNet(), x)
 
     def test_concat(self):
         input_a = Variable(torch.randn(BATCH_SIZE, 3), volatile=True)
         input_b = Variable(torch.randn(BATCH_SIZE, 3), volatile=True)
-        inputs = (toC(input_a), toC(input_b))
-        trace, _ = torch.jit.record_trace(toC(ConcatNet()), inputs, num_derivatives=0)
-        self.assertExpected(str(trace))
-        self.assertONNXExpected(trace.export(False), "pbtxt")
+        inputs = ((toC(input_a), toC(input_b)), )
+        self.exportTest(toC(ConcatNet()), inputs)
 
     def test_permute(self):
         x = Variable(torch.randn(BATCH_SIZE, 3, 10, 12), volatile=True)
-        trace, _ = torch.jit.record_trace(PermuteNet(), x)
-        self.assertExpected(str(trace))
-        self.assertONNXExpected(trace.export(False), "pbtxt")
+        self.exportTest(PermuteNet(), x)
 
     def test_srresnet(self):
         x = Variable(torch.randn(1, 3, 224, 224).fill_(1.0),
                      volatile=True)
-        trace, _ = torch.jit.record_trace(
-            toC(SRResNet(rescale_factor=4, n_filters=64, n_blocks=8)), toC(x))
-        self.assertExpected(str(trace))
-        self.assertONNXExpected(trace.export(False), "pbtxt")
+        self.exportTest(toC(SRResNet(rescale_factor=4, n_filters=64, n_blocks=8)), toC(x))
 
     @skipIfNoLapack
     def test_super_resolution(self):
         x = Variable(
             torch.randn(BATCH_SIZE, 1, 224, 224).fill_(1.0), volatile=True
         )
-        trace, _ = torch.jit.record_trace(
-            toC(SuperResolutionNet(upscale_factor=3)), toC(x))
-        self.assertExpected(str(trace))
-        self.assertONNXExpected(trace.export(False), "pbtxt")
+        self.exportTest(toC(SuperResolutionNet(upscale_factor=3)), toC(x))
 
     def test_alexnet(self):
         x = Variable(
             torch.randn(BATCH_SIZE, 3, 224, 224).fill_(1.0), volatile=True
         )
-        trace, _ = torch.jit.record_trace(toC(AlexNet()), toC(x))
-        self.assertExpected(str(trace))
-        self.assertONNXExpected(trace.export(False), "pbtxt")
+        self.exportTest(toC(AlexNet()), toC(x))
 
+    @unittest.skip("FeatureDropout symbolic not implemented yet")
     def test_mnist(self):
         x = Variable(torch.randn(BATCH_SIZE, 1, 28, 28).fill_(1.0),
                      volatile=True)
-        trace, _ = torch.jit.record_trace(toC(MNIST()), toC(x))
-        self.assertExpected(str(trace))
-        # FeatureDropout symbolic is not implemented yet
-        # self.assertONNXExpected(trace.export(False), "pbtxt")
+        self.exportTest(toC(MNIST()), toC(x))
 
     def test_vgg(self):
         # VGG 16-layer model (configuration "D")
         x = Variable(torch.randn(BATCH_SIZE, 3, 224, 224).fill_(1.0),
                      volatile=True)
         vgg16 = make_vgg16()
-        trace, _ = torch.jit.record_trace(toC(vgg16), toC(x))
-        self.assertExpected(str(trace), "16")
-        self.assertONNXExpected(trace.export(False), "16-pbtxt")
+        self.exportTest(toC(vgg16), toC(x), "16")
 
         # VGG 16-layer model (configuration "D") with batch normalization
         x = Variable(torch.randn(BATCH_SIZE, 3, 224, 224).fill_(1.0),
                      volatile=True)
         vgg16_bn = make_vgg16_bn()
-        trace, _ = torch.jit.record_trace(toC(vgg16_bn), toC(x))
-        self.assertExpected(str(trace), "16_bn")
-        self.assertONNXExpected(trace.export(False), "16_bn-pbtxt")
+        self.exportTest(toC(vgg16_bn), toC(x), "16_bn")
 
         # VGG 19-layer model (configuration "E")
         x = Variable(torch.randn(BATCH_SIZE, 3, 224, 224).fill_(1.0),
                      volatile=True)
         vgg19 = make_vgg19()
-        trace, _ = torch.jit.record_trace(toC(vgg19), toC(x))
-        self.assertExpected(str(trace), "19")
-        self.assertONNXExpected(trace.export(False), "19-pbtxt")
+        self.exportTest(toC(vgg19), toC(x), "19")
 
         # VGG 19-layer model (configuration 'E') with batch normalization
         x = Variable(torch.randn(BATCH_SIZE, 3, 224, 224).fill_(1.0),
                      volatile=True)
         vgg19_bn = make_vgg19_bn()
-        trace, _ = torch.jit.record_trace(toC(vgg19_bn), toC(x))
-        self.assertExpected(str(trace), "19_bn")
-        self.assertONNXExpected(trace.export(False), "19_bn-pbtxt")
+        self.exportTest(toC(vgg19_bn), toC(x), "19_bn")
 
     def test_resnet(self):
         # ResNet50 model
         x = Variable(torch.randn(BATCH_SIZE, 3, 224, 224).fill_(1.0),
                      volatile=True)
         resnet50 = ResNet(Bottleneck, [3, 4, 6, 3])
-        trace, _ = torch.jit.record_trace(toC(resnet50), toC(x))
-        self.assertExpected(str(trace), "50")
-        self.assertONNXExpected(trace.export(False), "50-pbtxt")
+        self.exportTest(toC(resnet50), toC(x), "50")
 
     def test_inception(self):
         x = Variable(
             torch.randn(BATCH_SIZE, 3, 299, 299).fill_(1.0), volatile=True)
-        trace, _ = torch.jit.record_trace(toC(Inception3()), toC(x))
-        self.assertExpected(str(trace), "3")
-        self.assertONNXExpected(trace.export(False), "3-pbtxt")
+        self.exportTest(toC(Inception3()), toC(x), "3")
 
     def test_squeezenet(self):
         # SqueezeNet: AlexNet-level accuracy with 50x fewer parameters and
@@ -157,18 +146,14 @@ class TestModels(TestCase):
         x = Variable(torch.randn(BATCH_SIZE, 3, 224, 224).fill_(1.0),
                      volatile=True)
         sqnet_v1_0 = SqueezeNet(version=1.1)
-        trace, _ = torch.jit.record_trace(toC(sqnet_v1_0), toC(x))
-        self.assertExpected(str(trace), "1_0")
-        self.assertONNXExpected(trace.export(False), "1_0-pbtxt")
+        self.exportTest(toC(sqnet_v1_0), toC(x), "1_0")
 
         # SqueezeNet 1.1 has 2.4x less computation and slightly fewer params
         # than SqueezeNet 1.0, without sacrificing accuracy.
         x = Variable(torch.randn(BATCH_SIZE, 3, 224, 224).fill_(1.0),
                      volatile=True)
         sqnet_v1_1 = SqueezeNet(version=1.1)
-        trace, _ = torch.jit.record_trace(toC(sqnet_v1_1), toC(x))
-        self.assertExpected(str(trace), "1_1")
-        self.assertONNXExpected(trace.export(False), "1_1-pbtxt")
+        self.exportTest(toC(sqnet_v1_1), toC(x), "1_1")
 
     def test_densenet(self):
         # Densenet-121 model
@@ -176,9 +161,7 @@ class TestModels(TestCase):
                      volatile=True)
         dense121 = DenseNet(num_init_features=64, growth_rate=32,
                             block_config=(6, 12, 24, 16))
-        trace, _ = torch.jit.record_trace(toC(dense121), toC(x))
-        self.assertExpected(str(trace), "121")
-        self.assertONNXExpected(trace.export(False), "121-pbtxt")
+        self.exportTest(toC(dense121), toC(x), "121")
 
     def test_dcgan(self):
         # note, could have more than 1 gpu
@@ -195,15 +178,11 @@ class TestModels(TestCase):
 
         netD.zero_grad()
         inputv = Variable(input)
-        trace, _ = torch.jit.record_trace(toC(netD), toC(inputv))
-        self.assertExpected(str(trace), "dcgan-netD")
-        self.assertONNXExpected(trace.export(False), "dcgan-netD-pbtxt")
+        self.exportTest(toC(netD), toC(inputv), "dcgan-netD")
 
         noise.resize_(bsz, nz, 1, 1).normal_(0, 1)
         noisev = Variable(noise)
-        trace, _ = torch.jit.record_trace(toC(netG), toC(noisev))
-        self.assertExpected(str(trace), "dcgan-netG")
-        self.assertONNXExpected(trace.export(False), "dcgan-netG-pbtxt")
+        self.exportTest(toC(netG), toC(noisev), "dcgan-netG")
 
     def run_word_language_model(self, model_name):
         # Args:
@@ -227,9 +206,7 @@ class TestModels(TestCase):
                          tied, batchsize)
         x = Variable(torch.LongTensor(10, batchsize).fill_(1),
                      volatile=False)
-        trace, _ = torch.jit.record_trace(model, x, model.hidden)
-        self.assertExpected(str(trace))
-        self.assertONNXExpected(trace.export(False), "pbtxt")
+        self.exportTest(model, (x, model.hidden))
 
     def test_word_language_model_RNN_TANH(self):
         model_name = 'RNN_TANH'
