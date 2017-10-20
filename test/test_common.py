878bfe0f76022b8a7486af5102cc6f20127166a9
Edward Z. Yang <ezyang@fb.com>
2017-10-20 11:28:44 -0700

    PyTorch is now on C++ autograd tracing.
    Also, operator tests moved to separate file.
    
    Signed-off-by: Edward Z. Yang <ezyang@fb.com>

--- test/test_common.py
+++ test/test_common.py
@@ -1,7 +1,10 @@
 import os.path
 import sys
+import torch
 
 _root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.insert(-1, os.path.join(_root_dir, "pytorch", "test"))
 
 from common import *
+
+torch.set_default_tensor_type('torch.FloatTensor')
