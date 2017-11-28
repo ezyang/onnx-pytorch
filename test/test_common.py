eb1464c92f3706bd1c2bb3d78a711d013b0a3390
Junjie Bai <bai@in.tum.de>
2017-11-28 16:07:21 -0500

    Skip some tests in Travis

--- test/test_common.py
+++ test/test_common.py
@@ -1,4 +1,6 @@
-import os.path
+import functools
+import os
+import unittest
 import sys
 import torch
 
@@ -8,3 +10,12 @@ sys.path.insert(-1, os.path.join(_root_dir, "pytorch", "test"))
 from common import *
 
 torch.set_default_tensor_type('torch.FloatTensor')
+
+
+def skipIfTravis(f):
+    @functools.wraps(f)
+    def wrapper(*args, **kwargs):
+        if os.getenv('TRAVIS'):
+            raise unittest.SkipTest('Skip In Travis')
+        return f(*args, **kwargs)
+    return wrapper
--- test/test_models.py
+++ test/test_models.py
@@ -11,7 +11,7 @@ from model_defs.srresnet import SRResNet
 from model_defs.dcgan import _netD, _netG, weights_init, bsz, imgsz, nz
 from model_defs.op_test import DummyNet, ConcatNet, PermuteNet, PReluNet
 
-from test_common import TestCase, run_tests, skipIfNoLapack
+from test_common import TestCase, run_tests, skipIfNoLapack, skipIfTravis
 
 import torch
 import torch.onnx
@@ -86,6 +86,7 @@ class TestModels(TestCase):
                      volatile=True)
         self.exportTest(toC(SRResNet(rescale_factor=4, n_filters=64, n_blocks=8)), toC(x))
 
+    @skipIfTravis
     @skipIfNoLapack
     def test_super_resolution(self):
         x = Variable(
@@ -105,6 +106,7 @@ class TestModels(TestCase):
                      volatile=True)
         self.exportTest(toC(MNIST()), toC(x))
 
+    @skipIfTravis
     def test_vgg(self):
         # VGG 16-layer model (configuration "D")
         x = Variable(torch.randn(BATCH_SIZE, 3, 224, 224).fill_(1.0),
